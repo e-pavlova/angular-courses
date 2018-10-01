bf5b3d3ac236125d2f8a9f8e75c81c21790e1718
Ekaterina Pavlova <34161127+e-pavlova@users.noreply.github.com>
2018-10-01 22:05:11 +0300

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { CourseItemComponent } from './course-item/course-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BreadcrumbsComponent,
+    CourseItemComponent
   ],
   imports: [
     BrowserModule
