c6429b8d4928a201a150f5164f394ca1ebd61fb9
Ekaterina Pavlova <34161127+e-pavlova@users.noreply.github.com>
2018-10-08 15:26:40 +0300

    Implement duration pipe

---
+++ src/app/shared/pipes/duration.pipe.ts
@@ -0,0 +1,25 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  name: 'duration'
+})
+
+export class DurationPipe implements PipeTransform {
+  public hours: number;
+  public minutes: number;
+
+  transform(time: number): string {
+    if (time < 60) {
+      this.minutes = time;
+
+      return `${this.minutes}min`;
+    } else {
+      this.hours = Math.floor(time / 60);
+      time = time - (this.hours * 60);
+    }
+    this.minutes = time;
+
+    return `${this.hours}h ${this.minutes}min`;
+  }
+}
+
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DurationPipe } from './pipes/duration.pipe';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    DurationPipe
   ],
   declarations: []
 })
