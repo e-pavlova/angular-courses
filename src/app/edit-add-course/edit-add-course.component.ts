c8c12bf106db6bef0649e793aa2dfb81af2c78f9
Ekaterina Pavlova <34161127+e-pavlova@users.noreply.github.com>
2018-10-15 10:57:01 +0300

    Add edit page

---
+++ src/app/edit-add-course/edit-add-course.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-add-course',
+  templateUrl: './edit-add-course.component.html',
+  styleUrls: ['./edit-add-course.component.css']
+})
+export class EditAddCourseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
