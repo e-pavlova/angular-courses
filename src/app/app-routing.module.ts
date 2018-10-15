8f39f24ddabd8c0380da17fe47ce482ac980ad3c
Ekaterina Pavlova <34161127+e-pavlova@users.noreply.github.com>
2018-10-15 12:03:53 +0300

    Add route for edit course page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { CoursesPageComponent } from './courses-page/courses-page.component';
 import { LoginComponent } from './login/login.component';
 import {NoContentComponent} from './no-content/no-content.component';
 import {CourseComponent} from './courses-page/course/course.component';
+import {EditAddCourseComponent} from './edit-add-course/edit-add-course.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/courses', pathMatch: 'full' },
   { path: 'courses', component: CoursesPageComponent },
   // { path: 'courses/:id', component: CourseComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'course/edit', component: EditAddCourseComponent },
   { path: '**', component: NoContentComponent }
 ];
 
