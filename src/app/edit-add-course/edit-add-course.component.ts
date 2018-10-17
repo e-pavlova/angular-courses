2c4083d0c129f00880107393e0954eb9007e1e5a
Ekaterina Pavlova <34161127+e-pavlova@users.noreply.github.com>
2018-10-17 10:59:56 +0300

    Add binding for edit page fields

--- src/app/edit-add-course/edit-add-course.component.ts
+++ src/app/edit-add-course/edit-add-course.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, ActivatedRouteSnapshot} from '@angular/router';
+import {CourseService} from '../core/services/course.service';
+import {Course} from '../courses-page/course/course';
 
 @Component({
   selector: 'app-edit-add-course',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditAddCourseComponent implements OnInit {
 
-  constructor() { }
+  public title: string;
+  public description: string;
+  public date: Date;
+  public duration: number;
+  public authors = '';
+
+  constructor(private route: ActivatedRoute,
+              private courseService: CourseService) {
+  }
 
   ngOnInit() {
+    const id: string = this.route.snapshot.params.id;
+    const currCourse: Course = this.courseService.getItemByID(parseInt(id, 10));
+
+    this.title = currCourse.title;
+    this.description = currCourse.description;
+    this.date = currCourse.date;
+    this.duration = currCourse.duration;
   }
 
 }
