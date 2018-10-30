177f889b65d83f21af1178e345bcb6194bd3fc52
Ekaterina Pavlova <34161127+e-pavlova@users.noreply.github.com>
2018-10-30 12:52:06 +0300

    Implement authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CoursesPageComponent } from './courses-page/courses-page.component';
 import { LoginComponent } from './login/login.component';
-import {NoContentComponent} from './no-content/no-content.component';
-import {CourseComponent} from './courses-page/course/course.component';
-import {EditCourseComponent} from './edit-course/edit-course.component';
-import {AddCourseComponent} from './add-course/add-course.component';
+import { NoContentComponent } from './no-content/no-content.component';
+import { CourseComponent } from './courses-page/course/course.component';
+import { EditCourseComponent } from './edit-course/edit-course.component';
+import { AddCourseComponent } from './add-course/add-course.component';
+import {AuthGuard} from './core/guards/authGuard';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/courses', pathMatch: 'full' },
-  { path: 'courses', component: CoursesPageComponent },
+  { path: '', redirectTo: '/courses', canActivate: [AuthGuard], pathMatch: 'full' },
+  { path: 'courses', component: CoursesPageComponent, canActivate: [AuthGuard] },
   // { path: 'courses/:id', component: CourseComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'course/:id/edit', component: EditCourseComponent },
-  { path: 'course/add', component: AddCourseComponent },
-  { path: '**', component: NoContentComponent }
+  { path: 'course/:id/edit', component: EditCourseComponent, canActivate: [AuthGuard] },
+  { path: 'course/add', component: AddCourseComponent, canActivate: [AuthGuard] },
+  { path: '**', component: NoContentComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
